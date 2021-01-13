a5ad63e549d0251e14a71967e3361159b247b032
booldook <booldook@gmail.com>
2021-01-13 17:47:30 +0900

    booldook

--- public/js/index.js
+++ public/js/index.js
@@ -39,11 +39,13 @@ function onShowDepth1() {
 }
 
 function onShowModal() {
+	$('html, body').css({'height': '100vh', 'overflow': 'hidden'});
 	$('.modal-container').addClass('active');
 	$('i.fa-arrow-left').hide();
 }
 
 function onHideModal() {
+	$('html, body').css({'height': 'auto', 'overflow': 'visible'});
 	$('.modal-container, .depth2').removeClass('active');
 }
 
